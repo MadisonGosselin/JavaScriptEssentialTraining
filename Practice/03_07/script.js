8e382943db0afe096ac58b2e4b5811594c3ca1e9
Madison Gosselin <madisoncgosselin@gmail.com>
2024-07-30 14:59:26 -0400

    Practice 3.07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,41 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const farm = {
+  acres: 100,
+  inhabited: true,
+  familyName: "Smith",
+
+  crop: "beans",
+  replant: function (crop) {
+    this.crop = crop;
+  },
+
+  animals: {
+    pigs: true,
+    horses: false,
+    chickens: true,
+    roosters: true,
+  },
+};
+
+console.log("Farm land:", farm);
+
+const car = {
+  stats: {
+    make: "Mazda",
+    model: "Miata",
+    year: "1990",
+    colour: "red",
+  },
+  repaint: function (colour) {
+    this.colour = colour;
+  },
+
+  gasLvl: 0.5,
+  tireAge: "2 years",
+  tireType: "Winter",
+};
+
+console.log("Car:", car);
