897ec4eba611c034f07193c19e5a53ec44d01fa3
Madison Gosselin <madisoncgosselin@gmail.com>
2024-08-01 10:58:13 -0400

    Practice 5.04

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,14 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+var log = document.querySelector(".page-header__heading");
+console.log("heading", log);
+
+log = document.querySelector("main");
+console.log("main", log);
+
+log = document.querySelectorAll(".backpack__features");
+console.log("ul", log);
+
+log = document.querySelector(".backpack__features");
+console.log("ul2", log);
