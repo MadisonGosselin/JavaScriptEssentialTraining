c4d51c722374fdbb08941e5446e62d847be4efca
Madison Gosselin <madisoncgosselin@gmail.com>
2024-08-06 13:27:47 -0400

    Practice 8.06

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,23 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+function newElement(eleType, data) {
+  const newElement = document.createElement(eleType);
+  newElement.innerHTML = data;
+
+  const addClass = () => {
+    document.querySelector("main " + eleType).classList.add("newEle");
+  };
+
+  // use helper function to add element then add class
+  addElement(newElement);
+  addClass();
+}
+
+const addElement = function (element) {
+  const main = document.querySelector("main");
+  main.append(element);
+};
+
+newElement("article", `<h1>Hello, this is a new article!</h1>`);
